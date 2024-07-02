5fbd8d1bd04b3563415753544b01ed42a39785bb
ElvyYang <eynglv@gmail.com>
2024-07-01 23:00:22 -0700

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material"
 
 import StacklineLogo from './StacklineLogo'
+import Logo from './stackline_logo.svg';
 
 const Header = (): JSX.Element => (
     <Box sx={{ backgroundColor: 'primary.main', width: "100%", height: '80px' }}>
-        {/* TODO: come back for this */}
-        {/* <StacklineLogo /> */}
+        <StacklineLogo fontSize="large" style={{ fill: 'white' }} sx={{ ml: 2, mt: 2.5 }} />
     </Box>
 );
 
